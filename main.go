a3e96a47bfbc7ae5e85e03adf9870f3f8bcd9711
Dangersohn <niklas.bette@web.de>
2017-05-23 16:32:31 +0200

    dev

--- main.go
+++ main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
+type Page struct {
+	Titel string
+	Text  []byte
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	text, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Titel: title, Text: text}, nil
+}
+
 func showChoice(w http.ResponseWriter, r *http.Request) {
+	p, _ := loadPage("choice1.1")
 	t, _ := template.ParseGlob("template/*.html")
-	t.ExecuteTemplate(w, "test", r.RequestURI)
+	t.ExecuteTemplate(w, string(p.Text), r.RequestURI)
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
